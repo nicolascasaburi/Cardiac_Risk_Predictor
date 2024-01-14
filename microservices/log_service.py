f80498dbf1f1a881bb3dc674ec64ba429b7516e3
Nicolás Casaburi <casaburinicolaslp@hotmail.com>
2024-01-13 21:11:24 -0300

    Se agrega validación de datos de entrada en el log_service, respuestas HTTP y también se modifica el servicio para recibir datos por JSON

--- microservices/log_service.py
+++ microservices/log_service.py
@@ -4,10 +4,7 @@ import time
 from flask import (
     Flask,
     request,
-    Response,
     abort,
-    jsonify,
-    current_app,
     g
 )  # se importa la librería principal de flask
 
@@ -15,22 +12,31 @@ def create_app(test_config=None):
     
     app = Flask(__name__, instance_relative_config=True)
     @app.route('/log_service',methods = ['POST'])
-    def authentication_request():
+    def log_service():
         """Microservicio que loguea las solicitudes del usuario"""
 
-        key = request.form.get("key")
-        nivel_colesterol = request.form.get("nivel_colesterol")
-        presion_arterial = request.form.get("presion_arterial")
-        azucar = request.form.get("azucar")
-        edad = request.form.get("edad")
-        sobrepeso = request.form.get("sobrepeso")
-        tabaquismo = request.form.get("tabaquismo")
-        resultado = request.form.get("resultado")
-        riesgo_cardiaco = request.form.get("riesgo_cardiaco")
-        tiempo_procesamiento = request.form.get("tiempo_procesamiento")
-        fecha = request.form.get("fecha")
+        datos_json = request.get_json()
+        key = datos_json.get("key")
+        nivel_colesterol = datos_json.get('nivel_colesterol')
+        presion_arterial = datos_json.get("presion_arterial")
+        azucar = datos_json.get("azucar")
+        edad = datos_json.get("edad")
+        sobrepeso = datos_json.get("sobrepeso")
+        tabaquismo = datos_json.get("tabaquismo")         
+        resultado = datos_json.get("resultado")
+        riesgo_cardiaco = datos_json.get("riesgo_cardiaco")
+        tiempo_procesamiento = datos_json.get("tiempo_procesamiento")
+        fecha = datos_json.get("fecha")
 
-        logueo(key,nivel_colesterol,presion_arterial,azucar,edad,sobrepeso,tabaquismo,str(float(resultado)),riesgo_cardiaco,fecha,tiempo_procesamiento)
+        # Se valida los datos de entrada
+        error = datos_usuario_validos(key,nivel_colesterol,presion_arterial,azucar,edad,sobrepeso,tabaquismo,resultado,riesgo_cardiaco,tiempo_procesamiento,fecha)
+        if error != '':
+            abort(404, error)
+
+        # Se guarda en la bitacora
+        error = logueo(key,nivel_colesterol,presion_arterial,azucar,edad,sobrepeso,tabaquismo,str(float(resultado)),riesgo_cardiaco,fecha,tiempo_procesamiento)
+        if error != '':
+            abort(502, error)
 
         return "La solicitud se registró correctamente en la bitacora"
     return app
@@ -49,4 +55,41 @@ def logueo(key:str, nivel_colesterol:str, presion_arterial:str, azucar:str, edad
     """Loguea una solicitud en la bitacora"""
 
     record = { "key" : key, "nivel_colesterol" : nivel_colesterol, "presion_arterial" : presion_arterial, "azucar" : azucar, "edad" : edad, "sobrepeso" : sobrepeso, "tabaquismo" : tabaquismo, "resultado" : resultado, "riesgo_cardiaco" : riesgo_cardiaco, "fecha" : fecha, "tiempo_procesamiento" : tiempo_procesamiento }
-    get_db().insert_one(record)
+    try:
+        get_db().insert_one(record)
+    except Exception as e:
+        return("Error al loguear en la bitacora:", e)
+    
+    return ''
+
+def datos_usuario_validos(key, nivel_colesterol, presion_arterial, azucar, edad, sobrepeso, tabaquismo, resultado, riesgo_cardiaco, tiempo_procesamiento, fecha):
+    """Verifica la existencia de los datos del paciente"""
+    error = ''
+
+    if key is None:
+        error = error + 'key '
+    if nivel_colesterol == '':
+        error = error + 'nivel_colesterol '
+    if presion_arterial is None:
+        error = error + 'presion_arterial '
+    if azucar is None:
+        error = error + 'azucar '
+    if edad is None:
+        error = error + 'edad '
+    if sobrepeso is None:
+        error = error + 'sobrepeso '
+    if tabaquismo is None:
+        error = error + 'tabaquismo '
+    if resultado is None:
+        error = error + 'resultado '
+    if riesgo_cardiaco is None:
+        error = error + 'riesgo_cardiaco '
+    if tiempo_procesamiento is None:
+        error = error + 'tiempo_procesamiento '
+    if fecha is None:
+        error = error + 'fecha '
+    if error != '':
+        error = "Faltan los parametros: " + error
+        return error
+
+    return error
