05c916bf7a6909b3c68d13863c8e4785608aca3b
Nicolás Casaburi <casaburinicolaslp@hotmail.com>
2024-01-14 15:57:17 -0300

    Updating main.py with database exception handling and new service paths

--- main.py
+++ main.py
@@ -1,62 +1,71 @@
 import os
+import sys
+import pymongo
+from pymongo import errors
 
-####### PUERTOS #######################
+####### PORTS #########################
 
-user_service_port = 5000
+gateway_service_port = 5000
 authentication_service_port = 5001
 prediction_service_port = 5002
 log_service_port = 5003
 
 #######################################
 
-#################### CONEXION A LA BD ########################
+#################### DB CONNECTION ###########################
 
 db_connection_string = "mongodb://mongoadmin:secret@localhost"
 
 ##############################################################
 
 def main():
-    """Inicio de la aplicación"""
+    """Start the application"""
     
-    cargar_db()
-    levantar_servicios()
+    load_db()
+    start_services()
 
-def cargar_db():
-    """Carga la Base de Datos de MongoDB con los usuarios"""
-  
-    import pymongo
-    dbClient = pymongo.MongoClient(db_connection_string)
-    db = dbClient['riesgo_cardiaco']
-    usuarios = [
-    {'key': '741f24cf76d772b15dcdd896d6044812', 'tipo': 'freemium'},
-    {'key': '7803f9b4f94ab605f48087da2c2a1627', 'tipo': 'premium'},
-    {'key': '2ed4bbc82dd29faeb4487092bdc535ed', 'tipo': 'freemium'},
-    {'key': '61ca6ffc6b94545a58a75ce0637ebf36', 'tipo': 'premium'},
-    {'key': '33d253c53e5739e7024a4f25abc81b22', 'tipo': 'freemium'},
-    {'key': 'fb2f370aa9053ca5bb107d888180f94a', 'tipo': 'premium'},
-    ]
-    for user in usuarios:
-        result = db['usuarios'].find_one(user)
-        if result is None:
-            db['usuarios'].insert_one(user)
+def load_db():
+    """Populate the database with users if they don't exist already"""
+    
+    try:
+        dbClient = pymongo.MongoClient(db_connection_string)
+        dbClient.list_databases()        
+        db = dbClient['cardiac_risk']
+        usuarios = [
+                    {'key': '741f24cf76d772b15dcdd896d6044812', 'type': 'freemium'},
+                    {'key': '7803f9b4f94ab605f48087da2c2a1627', 'type': 'premium'},
+                    {'key': '2ed4bbc82dd29faeb4487092bdc535ed', 'type': 'freemium'},
+                    {'key': '61ca6ffc6b94545a58a75ce0637ebf36', 'type': 'premium'},
+                    {'key': '33d253c53e5739e7024a4f25abc81b22', 'type': 'freemium'},
+                    {'key': 'fb2f370aa9053ca5bb107d888180f94a', 'type': 'premium'},
+        ]
+        for user in usuarios:
+            result = db['users'].find_one(user)
+            if result is None:
+                db['users'].insert_one(user)
+    except errors.ServerSelectionTimeoutError as e:
+        print(f"An error occurred while connecting to the MongoDB server: {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An error related to MongoDB has ocurred: {e}") 
 
-def levantar_servicios():
-    """Levanta todos los servicios con los puertos definidos"""
+def start_services():
+    """Start all services with defined ports"""
 
-    # Servicio Authentication
-    command = "cd microservices; . .venv/bin/activate; flask --app 'authentication_service:create_app(\""+db_connection_string+"\")' run --port "+str(authentication_service_port)+" &"
+    # Authentication service
+    command = "cd microservices/authentication_service; . .venv/bin/activate; flask --app 'authentication_service:create_app(\""+db_connection_string+"\")' run --port "+str(authentication_service_port)+" &"
     os.system(command)
 
-    # Servicio Prediction
-    command = "cd microservices; . .venv/bin/activate; flask --app prediction_service.py run --port "+str(prediction_service_port)+" &"
+    # Prediction service
+    command = "cd microservices/prediction_service; . .venv/bin/activate; flask --app prediction_service.py run --port "+str(prediction_service_port)+" &"
     os.system(command)
 
-    # Servicio Log
-    command = "cd microservices; . .venv/bin/activate; flask --app 'log_service:create_app(\""+db_connection_string+"\")' run --port "+str(log_service_port)+" &"
+    # Log service
+    command = "cd microservices/log_service; . .venv/bin/activate; flask --app 'log_service:create_app(\""+db_connection_string+"\")' run --port "+str(log_service_port)+" &"
     os.system(command)
 
-    # Servicio User
-    command = "cd microservices; . .venv/bin/activate; flask --app 'user_service:create_app("+str(authentication_service_port)+","+str(prediction_service_port)+","+str(log_service_port)+")' run --port "+str(user_service_port)+" &"
+    # Gateway_service
+    command = "cd microservices/gateway_service; . .venv/bin/activate; flask --app 'gateway_service:create_app("+str(authentication_service_port)+","+str(prediction_service_port)+","+str(log_service_port)+")' run --port "+str(gateway_service_port)+" &"
     os.system(command)
 
 main()
